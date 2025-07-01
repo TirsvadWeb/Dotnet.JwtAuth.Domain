7e1d39829f83f412b18c3f89f4a6c6ee52185751
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2025-07-01 12:23:49 +0800

    Add project files.

---
+++ src/TirsvadWeb.JwtAuth.Domain/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TirsvadWeb.JwtAuth.Domain.Entities;
+
+/// <summary>
+/// Represents an application user with JWT authentication support.
+/// Inherits from <see cref="IdentityUser"/> and adds properties for refresh token management.
+/// </summary>
+public class User : IdentityUser
+{
+    /// <summary>
+    /// Gets or sets the refresh token associated with the user.
+    /// </summary>
+    public string? RefreshToken { get; set; }
+
+    /// <summary>
+    /// Gets or sets the expiry time of the refresh token.
+    /// </summary>
+    public DateTime? RefreshTokenExpiryTime { get; set; }
+}
