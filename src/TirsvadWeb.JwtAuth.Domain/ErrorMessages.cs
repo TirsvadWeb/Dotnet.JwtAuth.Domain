7d444c80733040e240e92f34398511085ace603d
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2025-07-10 13:59:01 +0800

    Add ErrorMessages class for constants

---
+++ src/TirsvadWeb.JwtAuth.Domain/ErrorMessages.cs
@@ -0,0 +1,9 @@
+﻿namespace TirsvadWeb.JwtAuth.Domain;
+
+internal class ErrorMessages
+{
+    /// <summary>
+    /// Message format for refresh token base64 length validation.
+    /// </summary>
+    public const string RefreshTokenBase64Length = "RefreshToken must be a base64 string representing exactly {0} bytes.";
+}
